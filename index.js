afe738bb5c6055a6ee7f848fa6c9a3bcc798c016
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-10-03 15:46:24 -0400

    added hosting code to api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
 const server = require('./server');
-const port = 5000;
+const port = process.env.PORT|| 5000;
 
 server.listen(port, () => console.log(`\n** API on port ${port} **\n`));
